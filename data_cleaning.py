64a5abaaf761114e06668025b4863e8193b0b401
John Doe <johndoe@example.com>
2018-01-28 00:58:38 -0500

    Added feature to load session data in chunks and append data to CSV out of memory

--- data_cleaning.py
+++ data_cleaning.py
@@ -6,13 +6,6 @@ import numpy as np
 s = strftime("%H:%M:%S", gmtime())
 print(s)
 
-# LOAD FILES
-print('1/8 Load session data')
-# countries = pd.read_csv('/Users/David/Documents/Programming/Springboard/CareerTrack/Projects/Airbnb/data/countries.csv')
-# age_gender_bkts = pd.read_csv('/Users/David/Documents/Programming/Springboard/CareerTrack/Projects/Airbnb/data/age_gender_bkts.csv')
-#sessions_og = pd.read_csv('/Users/David/Documents/Programming/Springboard/CareerTrack/Projects/Airbnb/data/sessions.csv', index_col = 0)
-sessions_og = pd.read_csv('/Users/David/Documents/Programming/Springboard/CareerTrack/Projects/Airbnb/data/sessions.csv', index_col = 0, nrows=10000)
-
 # LOAD FUNCTIONS
 def list_of_lists_sums(list_of_lists):
     '''Takes in a list of equal lists and returns single list if values in lists > 0'''
@@ -33,104 +26,160 @@ def create_column_1_0(df, colnamelist):
     
     return df
 
-# One hot encoding loop
-import math
 
-ids = set(list(sessions_og.index)) # create list of all ids to iterate through 
-total_iterations = math.ceil(len(ids) / 100) # Creats toal number of iterations need for loop *no functionality
-ids = [x for x in ids if str(x) != 'nan'] #remoce the nan in ids
-ids = [ids[i:i+100] for i in range(0, len(ids), 100)] #makes id a lis
 
-full_df = pd.DataFrame()
-ticker = 0
-print('2/8 One hot encoding loop')
-for i in ids: 
-    print('{}/{}'.format(ticker, total_iterations))
-    ticker += 1 
-    sessions = sessions_og[sessions_og.index.isin(i)]
-    # CLEAN SESSION DATA 
-    # ONE HOT #ENCODNING FOR SESSIONS AFTER DROPPING TIME
+# ONE HOT ENCODING CHUNKS (44 min)
+# Loads session data in chunks of 5000/10567737, one hot encoded each chunk and add binary column then writes to CSV
+print('Load Data In Chunks of 100,000 to Create Dummies and Write to CSV, ~45 min')
+print(strftime("%H:%M:%S", gmtime()))
+ticker = 0 
+for chunk in pd.read_csv('/Users/David/Documents/Programming/Springboard/CareerTrack/Projects/Airbnb/data/Original/sessions.csv',chunksize=100000, index_col = 0): 
+    print('Chunk {}/106'.format(ticker))
+    # Create new csv file to append to 
+    pd.DataFrame().to_csv('/Users/David/Documents/Programming/Springboard/CareerTrack/Projects/Airbnb/data/Chunks/chunk_{}.csv'.format(ticker))
+
     # Drop time
-    sessions_dummies = sessions.drop('secs_elapsed', 1)
-    
-    #Create dummies 
-    sessions_dummies = pd.get_dummies(sessions_dummies,drop_first=True)
+    chunk_dummies = chunk.drop('secs_elapsed', 1)
 
-    # Loop through each id in sessions
-    user_ids = set(sessions.index)
-    sessions_ohe = pd.DataFrame()
-    
+    #Create dummies 
+    chunk_dummies = pd.get_dummies(chunk_dummies,drop_first=True)
+
+    # Loop through each id in sessions create empty df to append encoding to
+    user_ids = set(chunk.index)
+    chunk_ohe = pd.DataFrame()
+
     for i in user_ids:
-        df = sessions_dummies.loc[sessions_dummies.index == i] #Filters by id
+        df = chunk_dummies.loc[chunk_dummies.index == i] #Filters by id
         values = df.sum(axis=0) #Sums values vertically
         row = values.to_frame().T #Trasnposed data
         row['id'] = i
-        sessions_ohe = sessions_ohe.append(row) #Addes row to df
+        chunk_ohe = chunk_ohe.append(row) #Addes row to df
     
-    del sessions_dummies
-    # Reset index
-    sessions_ohe.set_index('id', inplace=True)
-
-    #MIN MAX MEAN SESSION TIME 
-    times = pd.DataFrame(sessions['secs_elapsed'])
-    sesh_times = pd.DataFrame()
+    del(chunk_dummies, user_ids)
     
-    for i in user_ids:
-        df = times.loc[times.index == i] #Filters by id
-        user_times = df['secs_elapsed']
-        row = pd.DataFrame([[min(user_times), max(user_times), 
-                             np.mean(user_times), np.std(user_times)]],
-                            columns=['session_time_min','session_time_max',
-                                     'session_time_mean', 'session_time_std'])
-        row['session_time_total'] = len(user_times)
-        row['session_count'] = np.sum(user_times)
-        row['id'] = i
-        sesh_times = sesh_times.append(row) #Addes row to df
+    # Reset index    
+    chunk_ohe.set_index('id', inplace=True)
     
-    del times
-    # Reset index
-    sesh_times.set_index('id', inplace=True)
-    
-    
-    # JOIN ONE HOT ENCODING WITH TIME SESSION DATA
-    sessions_clnd = sesh_times.join(sessions_ohe)
-    del sesh_times
-    del sessions_ohe
-
     # Create binary columns if a action/request/device is not 0
-    colnames_to_convert = list(sessions_clnd)[6:156]  
-    sessions_clnd = create_column_1_0(sessions_clnd, colnames_to_convert)
+    colnames_to_convert = list(chunk_ohe)[6:156]  
+    chunk_ohe = create_column_1_0(chunk_ohe, colnames_to_convert)    
+    del colnames_to_convert
+
+    # Write to apropriate chunk
+    with open('/Users/David/Documents/Programming/Springboard/CareerTrack/Projects/Airbnb/data/Chunks/chunk_{}.csv'.format(ticker), 'a') as f:
+        chunk_ohe.to_csv(f, header=True)
     
-    full_df = full_df.append(sessions_clnd)
-    
-print('3/8 End of OHE')
-# CREATE ADDTIONAL VARIBLES IN SESSION DATA 
-# Create column with the number of login devices BETTER WAY??
-devices = [list(full_df['device_type_Android Phone']),
-    list(full_df['device_type_Mac Desktop']),
-    list(full_df['device_type_Windows Desktop']),
-    list(full_df['device_type_iPad Tablet']),
-    list(full_df['device_type_iPhone'])]
+    ticker += 1
 
-full_df['devices_total'] = list_of_lists_sums(devices)
 
-del devices
 
-# CLEAN COLUMN NAMES
+# UNIFY ALL DATA BY RELOADING CHUNKS
+# Reload all data chunks and concat them together
+print('Reload Data Chunks and Unify ~3 min')
+print(strftime("%H:%M:%S", gmtime()))
+import glob
+path =r'/Users/David/Documents/Programming/Springboard/CareerTrack/Projects/Airbnb/data/Chunks'
+allFiles = glob.glob(path + "/*.csv")
+
+list_ = []
+for i in allFiles:
+    dfx = pd.read_csv(i,skiprows=[0])
+    list_.append(dfx)
+
+# Create Unified df
+print('Unify Data')
+sessions_clnd = pd.concat(list_, axis=0)
+
+# Reset Index 
+sessions_clnd.set_index('id', inplace=True)
+
 # replace spaces with _ in colnames
-full_df.columns = full_df.columns.str.lower().str.replace(' ', '_')
+sessions_clnd.columns = sessions_clnd.columns.str.lower().str.replace(' ', '_')
  
 # Reoder colnames ABC
-full_df = full_df.reindex_axis(sorted(full_df.columns), axis=1)
+sessions_clnd = sessions_clnd.reindex_axis(sorted(sessions_clnd.columns), axis=1)
 
-# Export full_df to csv
-print('4/8 Exporting full_df')
-full_df.to_csv('/Users/David/Documents/Programming/Springboard/CareerTrack/Projects/Airbnb/data/sessions_clnd.csv')
+# write to file
+sessions_clnd.to_csv('/Users/David/Documents/Programming/Springboard/CareerTrack/Projects/Airbnb/data/sessions_clnd.csv')
+del sessions_clnd
+
+
+
+
+# # CALC MIN MAX MEAN SESSION TIMES
+print('Relaod Session Data and Calculate Session Time Info')
+print(strftime("%H:%M:%S", gmtime()))
+ticker = 0
+ch_ticker = 0
+for chunk in pd.read_csv('/Users/David/Documents/Programming/Springboard/CareerTrack/Projects/Airbnb/data/Original/sessions.csv',chunksize=100000, index_col = 0):
+    print('{}/106'.format(ch_ticker))
+    times = pd.DataFrame(chunk['secs_elapsed'])
+    user_ids = set(times.index)
+    
+    for i in user_ids:
+        # print('{}/{}'.format(ticker, len(user_ids)))
+        if type(i) == str: 
+            df = times.loc[times.index == i] #Filters by id
+
+            user_times = list(df['secs_elapsed'])
+            del df
+            user_times = [x for x in user_times if str(x) != 'nan'] #remove nan
+
+            if user_times != []:
+                # print(np.std(user_times))
+                row_min = min(user_times)
+
+                row_max = max(user_times)
+                row_mean = np.mean(user_times)
+                row_sum = np.sum(user_times)
+                if len(user_times) > 1:
+                    row_std = np.std(user_times)
+                    row_len = len(user_times)  
+            else:
+                row_min = 0
+                row_max = 0
+                row_mean = 0
+                row_sum = 0
+                row_std = 0
+                row_len = 0
+            
+            row_id = i
+            
+            row = pd.DataFrame(data={'id': [row_id], 'session_time_min': [row_min], 
+                'session_time_max': [row_max], 'session_time_mean': [row_mean], 
+                'session_time_std': [row_std], 'session_time_total': [row_sum], 'session_count': [row_len]})  
+            if ticker == 0:
+                with open('/Users/David/Documents/Programming/Springboard/CareerTrack/Projects/Airbnb/data/times.csv', 'a') as f:
+                    row.to_csv(f, header=True, index=True)
+            else:
+                with open('/Users/David/Documents/Programming/Springboard/CareerTrack/Projects/Airbnb/data/times.csv', 'a') as f:
+                    row.to_csv(f, header=False, index=True)
+            ticker += 1
+    ch_ticker += 1
+    del times
+
+
+# MERGE TIME AND SESSION DATA TOGETHER
+print('Unify Time and Cleaned Session Data')
+print(strftime("%H:%M:%S", gmtime()))
+
+# Reload cleaned sessions
+sessions_clnd = pd.read_csv('/Users/David/Documents/Programming/Springboard/CareerTrack/Projects/Airbnb/data/sessions_clnd.csv')
+# sesh_times = pd.read_csv('/Users/David/Documents/Programming/Springboard/CareerTrack/Projects/Airbnb/data/times.csv')
+
+# Join sessions with time
+full_session = sesh_times.join(sessions_clnd)
+del(sessions_clnd, sesh_times)
+full_session.to_csv('/Users/David/Documents/Programming/Springboard/CareerTrack/Projects/Airbnb/data/full_session.csv')
+
 
 
 # CLEAN TRAIN USERS
+print('Clean User Data')
+print(strftime("%H:%M:%S", gmtime()))
+
 # Load
-train_users_2 = pd.read_csv('/Users/David/Documents/Programming/Springboard/CareerTrack/Projects/Airbnb/data/train_users_2.csv', index_col = 0) 
+train_users_2 = pd.read_csv('/Users/David/Documents/Programming/Springboard/CareerTrack/Projects/Airbnb/data/Original/train_users_2.csv', index_col = 0) 
 # Create column if there was a destination made
 destinations = train_users_2['country_destination']
 made_reservation = [0 if x == 'NDF' else 1 for x in destinations]
@@ -141,16 +190,18 @@ train_users_2['date_account_created'] = pd.to_datetime(train_users_2['date_accou
 train_users_2['date_first_booking'] = pd.to_datetime(train_users_2['date_first_booking'])
 train_users_2['elapsed_time'] = train_users_2['date_first_booking'] - train_users_2['date_account_created']
 
-# JOING SESSION DATA WITH USER DATA
-print('5/8 Joining tain on full_df')
-test_users = pd.read_csv('/Users/David/Documents/Programming/Springboard/CareerTrack/Projects/Airbnb/data/test_users.csv', index_col = 0)
-train = train_users_2.join(full_df)
-print('6/8 Joining test on full_df')
-test = test_users.join(full_df)
 
-print('7/8 Writing train data')
+
+# JOING SESSION DATA WITH USER DATA
+print('Join and Write Users')
+print(strftime("%H:%M:%S", gmtime()))
+test_users = pd.read_csv('/Users/David/Documents/Programming/Springboard/CareerTrack/Projects/Airbnb/data/Original/test_users.csv', index_col = 0)
+# full_session = pd.read_csv('/Users/David/Documents/Programming/Springboard/CareerTrack/Projects/Airbnb/data/full_session.csv')
+
+train = train_users_2.join(full_session)
+test = test_users.join(full_session)
+
 train.to_csv('/Users/David/Documents/Programming/Springboard/CareerTrack/Projects/Airbnb/data/train.csv')
-print('8/8 Writing test data')
 test.to_csv('/Users/David/Documents/Programming/Springboard/CareerTrack/Projects/Airbnb/data/test.csv')
 
 # Set end time and calc elapsed time 
